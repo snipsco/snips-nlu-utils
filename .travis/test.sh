28113dd78e8b8a62197ace873de1abb2fa034506
Tobor <tobor.spins@snips.net>
2018-02-19 20:42:45 +0100

    Fix typo

--- .travis/test.sh
+++ .travis/test.sh
@@ -8,6 +8,6 @@ cargo test --all || \
 echo "Python tests..."
 cd python
 . ${VENV_PATH}/bin/activate
-ssh-agent sh -c "ssh-add; pip install -e '.test' --verbose" || \
+ssh-agent sh -c "ssh-add; pip install -e '.[test]' --verbose" || \
         die "Failed to install test dependencies"
 tox
