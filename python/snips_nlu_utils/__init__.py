67a48224739a9394e6b636b861afc970053eaafc
Tobor <tobor.spins@snips.net>
2018-02-19 21:01:39 +0100

    Update missing package path

--- python/snips_nlu_utils/__init__.py
+++ python/snips_nlu_utils/__init__.py
@@ -1,2 +1,2 @@
-from ._nlu_utils_py import (tokenize, tokenize_light, compute_all_ngrams,
-                           normalize, remove_diacritics)
+from ._snips_nlu_utils_py import (tokenize, tokenize_light, compute_all_ngrams,
+                                  normalize, remove_diacritics)
