0215ce18e1d7b65b237abebb20e925b5b507c373
Adrien Ball <adrien.ball@snips.net>
2018-02-21 15:51:52 +0100

    Fix packaging issues

--- python/setup.py
+++ python/setup.py
@@ -16,6 +16,7 @@ packages = [p for p in find_packages() if "tests" not in p]
 PACKAGE_NAME = "snips_nlu_utils"
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 PACKAGE_PATH = os.path.join(ROOT_PATH, PACKAGE_NAME)
+README = os.path.join(ROOT_PATH, "README.rst")
 
 CARGO_FILE_PATH = os.path.join(ROOT_PATH, 'snips_nlu_utils_py', 'Cargo.toml')
 RUST_EXTENSION_NAME = 'snips_nlu_utils._snips_nlu_utils_py'
@@ -24,14 +25,28 @@ VERSION = "__version__"
 with io.open(os.path.join(PACKAGE_PATH, VERSION)) as f:
     version = f.readline().strip()
 
+with io.open(README, 'rt', encoding='utf8') as f:
+    readme = f.read()
+
 required = [
     "future==0.16.0"
 ]
 
 setup(name=PACKAGE_NAME,
+      description="NLU utils library for Snips NLU",
+      long_description=readme,
       version=version,
       author="Adrien Ball",
       author_email="adrien.ball@snips.ai",
+      license="Apache 2.0",
+      classifiers=[
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+      ],
       install_requires=required,
       rust_extensions=[RustExtension(RUST_EXTENSION_NAME, CARGO_FILE_PATH,
                                      **rust_kwargs)],
--- update_version.sh
+++ update_version.sh
@@ -4,4 +4,5 @@ source ./.travis/common.sh
 NEW_VERSION=$(parseRustVersion)
 echo "Updating versions to version ${NEW_VERSION}"
 perl -p -i -e "s/^version = \".*\"\$/version = \"$NEW_VERSION\"/g" */**/Cargo.toml
+perl -p -i -e "s/https:\/\/github\.com\/snipsco\/snips-nlu-utils\", tag = \".*\"/https:\/\/github\.com\/snipsco\/snips-nlu-utils\", tag = \"$NEW_VERSION\"/g" */**/Cargo.toml
 echo "$NEW_VERSION" > python/snips_nlu_utils/__version__
