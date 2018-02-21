84c827c794da5df47acfef20b46a95cfaf3ba93b
ClemDoum <clement.doumouro@gmaiil.com>
2018-02-21 10:26:50 +0100

    Remove publish script

--- .travis/test.sh
+++ .travis/test.sh
@@ -4,11 +4,10 @@ source .travis/common.sh
 echo "Rust tests..."
 export PATH="/usr/local/bin:$HOME/.cargo/bin:$PATH"
 cargo test --all || \
-        die "Rust test failed"
+        die "Rust tests failed"
 
 echo "Python tests..."
 cd python
-. ${VENV_PATH}/bin/activate
-pip install -e '.[test]' --verbose || \
-    die "Failed to install test dependencies"
-tox
+python -m pip install tox
+tox || \
+    die "Python tests failed"
