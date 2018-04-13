31be7714b8d2eebaab14ece137220f5afbcd2b20
Adrien Ball <adrien.ball@snips.net>
2018-04-13 15:14:01 +0200

    Improve update_version script and set version to 0.7.0-SNAPSHOT

--- update_version.sh
+++ update_version.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-source ./.travis/common.sh
 
-NEW_VERSION=$(parseRustVersion)
+NEW_VERSION=${1?"usage $0 <new version>"}
+
 echo "Updating versions to version ${NEW_VERSION}"
+perl -p -i -e "s/^version = \".*\"\$/version = \"$NEW_VERSION\"/g" Cargo.toml
 perl -p -i -e "s/^version = \".*\"\$/version = \"$NEW_VERSION\"/g" */**/Cargo.toml
-perl -p -i -e "s/https:\/\/github\.com\/snipsco\/snips-nlu-utils\", tag = \".*\"/https:\/\/github\.com\/snipsco\/snips-nlu-utils\", tag = \"$NEW_VERSION\"/g" */**/Cargo.toml
 echo "$NEW_VERSION" > python/snips_nlu_utils/__version__
+
+if [[ "${NEW_VERSION}" == "${NEW_VERSION/-SNAPSHOT/}" ]]
+then
+    perl -p -i -e "s/^snips-nlu-utils = .*\$/snips-nlu-utils = { git = \"https:\/\/github.com\/snipsco\/snips-nlu-utils\", tag = \"${NEW_VERSION}\" }/g" */**/Cargo.toml
+else
+    perl -p -i -e "s/^snips-nlu-utils = .*\$/snips-nlu-utils = { path = \"..\/..\" \}/g" */**/Cargo.toml
+fi
