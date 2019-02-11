3be5edab6a26973440029c317c5eae48c3208ca1
Matt Gathu <mattgathu@gmail.com>
2019-02-11 17:47:52 +0100

    updated tests

--- src/tests.rs
+++ src/tests.rs
@@ -31,7 +31,7 @@ const TEST_DATA: [(&'static str, i32); 7] = [
 fn dummy_trie() -> Trie {
     let mut t = Trie::new();
     for &(k, v) in &TEST_DATA {
-        t.insert(k, v.to_val());
+        t.insert(k.to_val(), v.to_val());
     }
     assert!(t.check_integrity());
 
@@ -41,60 +41,43 @@ fn dummy_trie() -> Trie {
 #[test]
 fn get_nonexistant() {
     let trie = dummy_trie();
-    assert!(trie.get("nonexistant").is_none());
-    assert!(trie.get("").is_none());
+    assert!(trie.get("nonexistant".to_val()).is_none());
+    assert!(trie.get("".to_val()).is_none());
+    assert!(trie.get("abcx".to_val()).is_none());
+    assert!(trie.get("x".to_val()).is_none());
 }
 
 #[test]
 fn unicode() {
     let mut trie = Trie::new();
-    trie.insert("bär", vec![1]);
-    trie.insert("bären", vec![2]);
+    trie.insert("bär".to_val(), vec![1]);
+    trie.insert("bären".to_val(), vec![2]);
 
-    assert_eq!(*trie.get("bär").unwrap(), vec![1]);
-}
-
-#[test]
-fn empty_key() {
-    let mut trie = dummy_trie();
-    trie.insert("", vec![1]);
-    assert_eq!(*trie.get("").unwrap(), vec![1]);
-}
-
-#[test]
-fn insert() {
-    let trie = dummy_trie();
-
-    for &(key, val) in &TEST_DATA {
-        assert_eq!(*trie.get(key).unwrap(), val.to_val());
-    }
-
-    assert!(trie.check_integrity());
-    assert_eq!(trie.len(), TEST_DATA.len());
+    assert_eq!(*trie.get("bär".to_val()).unwrap(), vec![1]);
 }
 
 #[test]
 fn insert_replace() {
     let mut trie = Trie::new();
-    assert_eq!(trie.insert("haskell", vec![18]), None);
+    assert_eq!(trie.insert("haskell".to_val(), vec![18]), None);
     let length = trie.len();
-    assert_eq!(trie.insert("haskell", vec![36]), Some(vec![18]));
+    assert_eq!(trie.insert("haskell".to_val(), vec![36]), Some(vec![18]));
     assert_eq!(trie.len(), length);
 }
 
 #[test]
 fn insert_similar() {
     let mut trie = Trie::new();
-    trie.insert("a", vec![1, 4]);
-    trie.insert("p", vec![1, 3]);
-    assert_eq!(trie.get("a"), Some(&vec![1, 4]));
-    assert_eq!(trie.get("p"), Some(&vec![1, 3]));
-    trie.insert("a", vec![1, 5]);
-    assert_eq!(trie.get("a"), Some(&vec![1, 5]));
+    trie.insert("a".to_val(), vec![1, 4]);
+    trie.insert("p".to_val(), vec![1, 3]);
+    assert_eq!(trie.get("a".to_val()), Some(&vec![1, 4]));
+    assert_eq!(trie.get("p".to_val()), Some(&vec![1, 3]));
+    trie.insert("a".to_val(), vec![1, 5]);
+    assert_eq!(trie.get("a".to_val()), Some(&vec![1, 5]));
     assert_eq!(trie.len(), 2);
-    trie.remove("a");
+    trie.remove("a".to_val());
     assert_eq!(trie.len(), 1);
-    assert_eq!(trie.get("a"), None);
+    assert_eq!(trie.get("a".to_val()), None);
 }
 
 #[test]
@@ -103,14 +86,24 @@ fn remove() {
 
     // Remove.
     for &(key, val) in &TEST_DATA {
-        let res = trie.remove(key);
-        assert_eq!(res, Some(val.to_val()));
+        let res = trie.remove(key.to_val());
+        assert_eq!(
+            res,
+            Some(val.to_val()),
+            "{}",
+            format!("key: {:?} returned None \n {:#?}", key.to_val(), trie)
+        );
         assert!(trie.check_integrity());
     }
 
+    assert!(trie.is_empty());
+
     // Check non-existance.
     for &(key, _) in &TEST_DATA {
-        assert!(trie.get(key).is_none());
+        assert!(
+            trie.get(key.to_val()).is_none(),
+            format!("key: `{:?}` is not None", &key)
+        );
     }
 }
 
@@ -118,9 +111,9 @@ fn remove() {
 fn remove_simple() {
     let mut trie = Trie::new();
 
-    trie.insert("HELL", vec![66]);
-    trie.insert("HELLO", vec![77]);
-    let val = trie.remove("HELLO");
+    trie.insert("HELL".to_val(), vec![66]);
+    trie.insert("HELLO".to_val(), vec![77]);
+    let val = trie.remove("HELLO".to_val());
     assert_eq!(val, Some(vec![77]));
 }
 
@@ -128,68 +121,65 @@ fn remove_simple() {
 fn remove_plus_insertion() {
     let mut trie = Trie::new();
 
-    trie.insert("HELL", vec![66]);
-    trie.insert("HELLO", vec![77]);
-    let val = trie.remove("HELLO");
-    trie.insert("HELLO", vec![88]);
+    trie.insert("HELL".to_val(), vec![66]);
+    trie.insert("HELLO".to_val(), vec![77]);
+    let val = trie.remove("HELLO".to_val());
+    trie.insert("HELLO".to_val(), vec![88]);
     assert_eq!(val, Some(vec![77]));
-    assert_eq!(trie.get("HELLO"), Some(&vec![88]));
+    assert_eq!(trie.get("HELLO".to_val()), Some(&vec![88]));
 }
 
 #[test]
 fn remove_non_existent() {
     let mut trie = Trie::new();
 
-    trie.insert("acab", vec![1]);
+    trie.insert("acab".to_val(), vec![1]);
 
-    assert_eq!(trie.remove("abc"), None);
-    assert_eq!(trie.remove("acaba"), None);
-    assert_eq!(trie.remove("a"), None);
-    assert_eq!(trie.remove(""), None);
+    assert_eq!(trie.remove("abc".to_val()), None);
+    assert_eq!(trie.remove("acaba".to_val()), None);
+    assert_eq!(trie.remove("a".to_val()), None);
+    assert_eq!(trie.remove("".to_val()), None);
     assert_eq!(trie.len(), 1);
 
-    trie.insert("acaz", vec![1]);
+    trie.insert("acaz".to_val(), vec![1]);
 
-    assert_eq!(trie.remove("acb"), None);
-    assert_eq!(trie.remove("acaca"), None);
-    assert_eq!(trie.remove("aca"), None);
+    assert_eq!(trie.remove("acb".to_val()), None);
+    assert_eq!(trie.remove("acaca".to_val()), None);
+    assert_eq!(trie.remove("aca".to_val()), None);
     assert_eq!(trie.len(), 2);
 }
 
 #[test]
 fn get_prefix_bug() {
     let mut trie = Trie::new();
-    trie.insert("abdc", vec![5]);
-    trie.insert("abde", vec![6]);
-    assert!(trie.get("abc").is_none());
+    trie.insert("abdc".to_val(), vec![5]);
+    trie.insert("abde".to_val(), vec![6]);
+    assert!(trie.get("abc".to_val()).is_none());
 }
 
 #[test]
 fn root_replace_bug() {
     let mut trie = Trie::new();
-    trie.insert("a", "".to_val());
-    trie.insert("p", "".to_val());
-    dbg!(&trie);
-    dbg!(&trie.len());
-    trie.remove("a");
-    dbg!(&trie.len());
+    trie.insert("a".to_val(), "".to_val());
+    trie.insert("p".to_val(), "".to_val());
+    trie.remove("a".to_val());
     assert_eq!(trie.len(), 1);
-    trie.remove("p");
+    trie.remove("p".to_val());
     assert_eq!(trie.len(), 0);
 }
 
 #[test]
 fn test_get_borrow() {
     let mut trie = Trie::new();
-    trie.insert("/boot".to_string(), "dir".to_val());
-    assert_eq!(*trie.get("/boot").unwrap(), "dir".to_val());
+    trie.insert("/boot".to_val(), "dir".to_val());
+    assert_eq!(*trie.get("/boot".to_val()).unwrap(), "dir".to_val());
 }
 
 #[test]
 fn test_remove_borrow() {
     let mut trie = Trie::new();
-    trie.insert("/boot".to_string(), "dir".to_val());
-    assert_eq!(trie.remove("/boot").unwrap(), "dir".to_val());
+    trie.insert("/boot".to_val(), "dir".to_val());
+    assert_eq!(trie.remove("/boot".to_val()).unwrap(), "dir".to_val());
 }
 
 #[test]
@@ -206,4 +196,6 @@ fn test_trie_map() {
     assert_eq!(map.get("/boot"), Some("dir".to_string()));
     map.insert("gamma  rho", "sigma");
     assert_eq!(map.get("gamma  rho"), Some("sigma".to_string()));
+    map.insert("gamma", "sigma  sigma");
+    assert_eq!(map.get("gamma"), Some("sigma  sigma".to_string()));
 }
