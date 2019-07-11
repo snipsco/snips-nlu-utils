c8e13847c35b4ac86dfd209e47e7c9abfd4c84b3
Adrien Ball <adrien.ball@snips.ai>
2019-07-11 18:33:06 +0200

    Fix dependencies

--- python/snips_nlu_utils/__init__.py
+++ python/snips_nlu_utils/__init__.py
@@ -1,3 +1,3 @@
-from snips_nlu_utils.string import get_shape, hash_str, normalize, \
-    remove_diacritics
+from snips_nlu_utils.string import (
+    get_shape, hash_str, normalize, remove_diacritics)
 from snips_nlu_utils.token import compute_all_ngrams, tokenize, tokenize_light
